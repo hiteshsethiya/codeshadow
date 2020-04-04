0815fff7b582027869ae7f28871d969f8177b47a
hitesh sethiya <hiteshsethiya@gmail.com>
2020-04-05 02:33:01 +0530

    challenge problem 2 completed

--- src/main/java/com/hitesh/test/hackerrank/challenge/MeetupSchedule.java
+++ src/main/java/com/hitesh/test/hackerrank/challenge/MeetupSchedule.java
@@ -1,5 +1,7 @@
 package com.hitesh.test.hackerrank.challenge;
 
+import java.util.*;
+
 public class MeetupSchedule {
 
     /*
@@ -64,4 +66,85 @@ public class MeetupSchedule {
      * The owner can only meet 2 investors out of 3: I-0 in day 1, I-2in day 2.
      */
 
+    static class Investor {
+        int idx;
+        int start;
+        int end;
+
+        public Investor(int idx, int start, int end) {
+            this.idx = idx;
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Investor investor = (Investor) o;
+            return start == investor.start &&
+                    end == investor.end;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(start, end);
+        }
+    }
+
+    public static int countMeetings(List<Integer> firstDay, List<Integer> lastDay) {
+        // Write your code here
+        if (firstDay.isEmpty() || firstDay.size() != lastDay.size()) return 0;
+        List<Investor> investors = new ArrayList<>();
+        for (int i = 0; i < firstDay.size(); ++i) {
+            investors.add(new Investor(i, firstDay.get(i), lastDay.get(i)));
+        }
+        investors.sort((a, b) -> {
+            if (a.end == b.end) return Integer.compare(a.start, b.start);
+            return Integer.compare(a.end, b.end);
+        });
+        Set<Integer> meetingDays = new HashSet<>();
+        for(Investor i : investors) {
+            for(int s = i.start; s <= i.end; ++s) {
+                if(!meetingDays.contains(s)) {
+                    meetingDays.add(s);
+                    break;
+                }
+            }
+        }
+        return meetingDays.size();
+    }
+
+    public static void execute(Integer[] a, Integer[] q, int expO) {
+        List<Integer> investorsStartDay = Arrays.asList(a);
+        List<Integer> investorsEndDay = Arrays.asList(q);
+        System.out.println("s: " + investorsStartDay);
+        System.out.println("e: " + investorsEndDay);
+        int answer = countMeetings(investorsStartDay, investorsEndDay);
+        System.out.println("a: " + answer);
+        System.out.println("e: " + expO);
+        System.out.println(answer == expO);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = new Integer[]{1, 2, 2, 3, 3, 3};
+        Integer[] q = new Integer[]{2, 2, 2, 3, 4, 4};
+        int exp = 4;
+        execute(a, q, exp);
+
+        a = new Integer[]{1, 2, 1, 2, 2};
+        q = new Integer[]{3, 2, 1, 3, 3};
+        exp = 3;
+        execute(a, q, exp);
+
+        a = new Integer[]{1, 1, 2, 2, 2, 3, 6, 7, 7, 8, 9, 10, 11, 12};
+        q = new Integer[]{1, 1, 2, 2, 3, 3, 7, 7, 8, 9, 9, 10, 11, 12};
+        exp = 10;
+        execute(a, q, exp);
+    }
+
 }
+
+
+
